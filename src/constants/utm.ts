a682673cef6fb3fab0119dd806ae695345bc62f9
dgavrichkov <dgavrichkiv32@gmail.con>
2024-11-29 02:17:07 +0400

    add utm marks to tg links

---
+++ src/constants/utm.ts
@@ -0,0 +1,13 @@
+export const UTM_MAP = {
+  common: "utm_source---yandex__utm_medium---cpc__utm_campaign---rsysitecommom",
+  skazka: "utm_source---yandex__utm_medium---direct__utm_campaign---skazka",
+  strateg: "utm_source---yandex__utm_medium---direct__utm_campaign---stategy",
+  fighting: "utm_source---yandex__utm_medium---direct__utm_campaign---fight",
+  bogi: "utm_source---yandex__utm_medium---direct__utm_campaign---bogi",
+  korol: "utm_source---yandex__utm_medium---direct__utm_campaign---royalbattle",
+  dnd: "utm_source---yandex__utm_medium---direct__utm_campaign---dnd",
+  sloveska: "utm_source---yandex__utm_medium---direct__utm_campaign---chonici",
+  chronicles: "utm_source---yandex__utm_medium---cpc__utm_campaign---rsydirect",
+};
+
+export const UTM_PRFX = "?start=";
